e4deec0b276788dfb358029a56e123380a99ead9
huntdfl <hdougherty91@gmail.com>
2021-10-11 12:30:41 -0400

    condensed info, added carousel

--- pcg.js
+++ pcg.js
@@ -1,9 +1,27 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
+var slidePosition = 0;
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
+SlideShow();
+
+ 
+
+function SlideShow() {
+
+  var i;
+
+  var slides = document.getElementsByClassName("Containers");
+
+  for (i = 0; i < slides.length; i++) {
+
+    slides[i].style.display = "none";
+
+  }
+
+  slidePosition++;
+
+  if (slidePosition > slides.length) {slidePosition = 1}
+
+  slides[slidePosition-1].style.display = "block";
+
+  setTimeout(SlideShow, 2000); // Change image every 2 seconds
+
+} 
